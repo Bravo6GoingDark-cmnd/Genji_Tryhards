74b36ae9929015e93bb0d3e9d3839dd54e4f9a61
Tejeeswar Reddy <tejeeswarreddya@gmail.com>
2025-01-18 15:42:55 -0600

    Create task.ps1

---
+++ task.ps1
@@ -0,0 +1,15 @@
+# Function to disable scheduled tasks
+Function Disable-Tasks {
+    Unregister-ScheduledTask -TaskPath *Bluetooth* -ErrorAction SilentlyContinue
+    Unregister-ScheduledTask -TaskPath *Location* -ErrorAction SilentlyContinue
+    Unregister-ScheduledTask -TaskPath *Maps* -ErrorAction SilentlyContinue
+    Unregister-ScheduledTask -TaskPath *UPnP* -ErrorAction SilentlyContinue
+    Unregister-ScheduledTask -TaskPath '*Plug and Play*' -ErrorAction SilentlyContinue
+    Unregister-ScheduledTask -TaskPath '*Windows Error Reporting*' -ErrorAction SilentlyContinue
+}
+
+# Export the function so it can be used after importing the module
+Export-ModuleMember -Function Disable-Tasks
+
+# code calls the function to disable tasks right after loading the script
+Disable-Tasks
