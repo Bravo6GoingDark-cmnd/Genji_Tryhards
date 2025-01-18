7cd3aea8e11b5f8d449ad2306a9a083222b6f482
Tejeeswar Reddy <tejeeswarreddya@gmail.com>
2025-01-18 15:22:58 -0600

    Update Interestingfiles.ps1

--- Interestingfiles.ps1
+++ Interestingfiles.ps1
@@ -1,3 +1,3 @@
 #processes that have bigger loads
-    'grabbing dem interesting process'
-    Get-Process | Where-Object {$_.WorkingSet -gt 20000000} > scripterino\interestingprocess.txt
+'grabbing dem interesting process'
+Get-Process | Where-Object {$_.WorkingSet -gt 20000000} > scripterino\interestingprocess.txt
