7213f9e688fb72e64a5529c5f50344a3272259dc
Tejeeswar Reddy <tejeeswarreddya@gmail.com>
2025-01-20 21:57:11 -0600

    Update Interestingfiles.ps1

--- Interestingfiles.ps1
+++ Interestingfiles.ps1
@@ -1,3 +1,9 @@
-#processes that have bigger loads
-'grabbing dem interesting process'
-Get-Process | Where-Object {$_.WorkingSet -gt 20000000} > scripterino\interestingprocess.txt
+#CODE CHECKS PASSED!!!
+# Create the folder "scripterino" if it doesn't already exist
+$folderPath = ".\scripterino"
+if (-not (Test-Path -Path $folderPath)) {
+    New-Item -Path $folderPath -ItemType Directory
+}
+
+# Grab processes with a working set > 20MB and save to interestingprocess.txt in the "scripterino" folder
+Get-Process | Where-Object {$_.WorkingSet -gt 20000000} > "$folderPath\interestingprocess.txt"
