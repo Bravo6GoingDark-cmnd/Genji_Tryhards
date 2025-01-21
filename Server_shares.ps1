5143f927bac6ee9e81e385c6d11af63316add927
Tejeeswar Reddy <tejeeswarreddya@gmail.com>
2025-01-20 21:58:06 -0600

    Update Server_shares.ps1

--- Server_shares.ps1
+++ Server_shares.ps1
@@ -1,13 +1,24 @@
+#CODE CHECKS PASSED!!!
 $user = 'ADJUST'
-#grabbing network shares check if irregular
-'grabbing shares bb'
-net share > scripterino\shares.txt
 
-#flush DNS
+# Create the "scripterino" folder if it doesn't exist
+$folderPath = "C:\Users\$user\Desktop\scripterino"
+if (-not (Test-Path -Path $folderPath)) {
+    New-Item -Path $folderPath -ItemType Directory
+}
+
+# Grabbing network shares and checking if irregular, output to shares.txt
+'grabbing shares bb'
+net share > "$folderPath\shares.txt"
+
+# Flush DNS cache
 'flushing dns cache'
 ipconfig /flushdns
 
-#Grabbing hosts file
-'grabbing hosts file'	
-New-Item -Path C:\Users\$user\Desktop\scripterino\hosts -ItemType directory
-Get-ChildItem -Path "C:\Windows\System32\drivers\etc\hosts" | Copy-Item -Destination C:\Users\$user\Desktop\scripterino\hosts
+# Grabbing hosts file
+'grabbing hosts file'
+$hostsFolder = "$folderPath\hosts"
+if (-not (Test-Path -Path $hostsFolder)) {
+    New-Item -Path $hostsFolder -ItemType Directory
+}
+Copy-Item -Path "C:\Windows\System32\drivers\etc\hosts" -Destination $hostsFolder
