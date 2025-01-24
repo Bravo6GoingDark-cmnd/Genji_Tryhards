32369304523b79ed2aaa3754d0f097ecdb21dba7
Tejeeswar Reddy <tejeeswarreddya@gmail.com>
2025-01-23 20:42:09 -0600

    Create reg_keys.bat

---
+++ Backup/reg_keys.ps1
@@ -0,0 +1,6 @@
+rem Manage Registry Keys
+echo Configuring registry keys...
+reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v AutoInstallMinorUpdates /t REG_DWORD /d 1 /f
+reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v NoAutoUpdate /t REG_DWORD /d 0 /f
+reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v AUOptions /t REG_DWORD /d 4 /f
+echo Finished reg keys
